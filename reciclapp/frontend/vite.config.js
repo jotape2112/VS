259da1c9be6e44f9c81bfa9b09455644568ba613
jotape2112 <juanpablo.jorquera47@gmail.com>
2025-10-22 16:39:23 -0300

    Update vite.config.js

--- reciclapp/frontend/vite.config.js
+++ reciclapp/frontend/vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
           type: "image/png",
         },
         {
-          src: "/icon-512x512.png",
+          src: "/icons/icon-512x512.png",
           sizes: "512x512",
           type: "image/png",
         },
